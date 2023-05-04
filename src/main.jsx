31e0db5934f2cec5106c103a94fbc730daa851ea
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-05-04 23:43:15 +0600

    added

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import AuthProvider from "./provider/AuthProvider";
+import router from "./routes/Routes.jsx";
+import 'react-toastify/dist/ReactToastify.css';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
